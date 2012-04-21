4d9a93a57a86ed60037d300dae2d4ddf65eff0d7
Brandon Cook <phylake@gmail.com>
2012-04-21 15:50:41 -0700

    added example

---
+++ examples/sample.rb
@@ -0,0 +1,15 @@
+_ ['.list_layout'] {
+  _ ['.list_style'] {
+    _ ['input'] {
+      width '100%'
+      margin_bottom '.5em'
+    }
+
+    padding '1em'
+    background '#F5F5F5'
+
+    r = 8
+    border 'solid', 1, '#eee', r, r, r, r
+    drop_shadow 1, 1, 1, '#fafafa', true
+  }
+}
